7435fff57f4ed19171dfc167f12e52708676ceb0
hanyoseob <hanyoseob@kaist.ac.kr>
2020-02-14 10:56:25 -0700

    Upload init

---
+++ eval.py
@@ -0,0 +1,155 @@
+## 라이브러리를 추가하기
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from torchvision import transforms, datasets
+
+## 트레이닝 파라메터를 설정하기
+lr = 1e-3
+batch_size = 64
+num_epoch = 10
+
+ckpt_dir = './checkpoint/'
+log_dir = './log/'
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+## 네트워크를 구축하기
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5, stride=1, padding=0, bias=True)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        self.relu1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5, stride=1, padding=0, bias=True)
+        self.drop2 = nn.Dropout2d(p=0.5)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+        self.relu2 = nn.ReLU()
+
+        self.fc1 = nn.Linear(in_features=320, out_features=50, bias=True)
+        self.relu1_fc1 = nn.ReLU()
+        self.drop1_fc1 = nn.Dropout2d(p=0.5)
+
+        self.fc2 = nn.Linear(in_features=50, out_features=10, bias=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.relu1(x)
+
+        x = self.conv2(x)
+        x = self.drop2(x)
+        x = self.pool2(x)
+        x = self.relu2(x)
+
+        x = x.view(-1, 320)
+
+        x = self.fc1(x)
+        x = self.relu1_fc1(x)
+        x = self.drop1_fc1(x)
+
+        x = self.fc2(x)
+
+        return x
+
+## 네트워크를 저장하거나 불러오는 함수 작성하기
+def save(ckpt_dir, net, optim, epoch):
+    if not os.path.exists(ckpt_dir):
+        os.makedirs(ckpt_dir)
+
+    torch.save({'net': net.state_dict(), 'optim': optim.state_dict()},
+               './%s/model_epoch%d.pth' % (ckpt_dir, epoch))
+
+    print('model_epoch%d.pth is saved.' % epoch)
+
+def load(ckpt_dir, net, optim):
+    ckpt_lst = os.listdir(ckpt_dir)
+    ckpt_lst.sort()
+
+    dict_model = torch.load('./%s/%s' % (ckpt_dir, ckpt_lst[-1]))
+
+    net.load_state_dict(dict_model['net'])
+    optim.load_state_dict(dict_model['optim'])
+
+    print('%s is loaded.' % ckpt_lst[-1])
+
+    return net, optim
+
+## 학습 시킬 데이터를 불러오기
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
+
+dataset = datasets.MNIST(root='./', train=False, download=True, transform=transform)
+loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+
+num_data = len(loader.dataset)
+num_batch = round(num_data / batch_size)
+
+## 네트워크를 생성하기
+net = Net()
+params = net.parameters()
+
+## 손실함수 등을 설정하기
+fn_loss = nn.CrossEntropyLoss().to(device)
+fn_pred = lambda output: torch.softmax(output, dim=1)
+fn_acc = lambda pred, label: ((pred.max(dim=1)[1] == label).type(torch.float)).mean()
+
+optim = torch.optim.Adam(params=params, lr=lr)
+
+writer = SummaryWriter(log_dir=log_dir)
+
+## 네트워크를 불러오기
+net, optim = load(ckpt_dir=ckpt_dir, net=net, optim=optim)
+
+## 테스트를 시작하기
+# for epoch in range(1, num_epoch + 1):
+    # net.train()
+with torch.no_grad():
+    net.eval()
+
+    loss_arr = []
+    acc_arr = []
+
+    for batch, (input, label) in enumerate(loader, 1):
+        # forward propagation 하기
+        input = input.to(device)
+        label = label.to(device)
+
+        output = net(input)
+        pred = fn_pred(output)
+
+        # backward propagation
+        # optim.zero_grad()
+
+        loss = fn_loss(output, label)
+        # loss.backward()
+
+        acc = fn_acc(pred, label)
+
+        # optim.step()
+
+        # 손실함수를 계산하기
+        loss_arr += [loss.item()]
+        acc_arr += [acc.item()]
+
+        # print('TRAIN: EPOCH %d/%d | BATCH %04d/%04d | LOSS: %.4f | ACC: %.4f' %
+        #       (epoch, num_epoch, batch, num_batch, np.mean(loss_arr), np.mean(acc_arr)))
+
+        print('EVAL: BATCH %04d/%04d | LOSS: %.4f | ACC: %.4f' %
+              (batch, num_batch, np.mean(loss_arr), np.mean(acc_arr)))
+
+    print('FINAL EVAL: LOSS: %.4f | ACC: %.4f' % (np.mean(loss_arr), np.mean(acc_arr)))
+
+    # write log
+    # writer.add_scalar('loss', np.mean(loss_arr), epoch)
+    # writer.add_scalar('acc', np.mean(acc_arr), epoch)
+
+    # Save network & optimizer
+    # save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
+
+# writer.close()
